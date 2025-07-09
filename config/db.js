3e571fd4eebe272b67b5377fd026e79b05c65e47
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-09 23:49:14 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const MONGO_URI = process.env.MONGO_URI;
-
     await mongoose.connect(MONGO_URI, {
       dbName: 'job-board',
     });
