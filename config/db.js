8bea5437c4f8c7dceeef910e7c5f65bf957342b7
Vishal Singhaniya <152794577+singhaniya0411@users.noreply.github.com>
2025-07-09 23:51:18 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+
+    const MONGO_URI = "mongodb+srv://vishalsinghaniya:RlMh6fV0uSTFTV9t@job-board-db.tf3vkap.mongodb.net/?retryWrites=true&w=majority&appName=job-board-db";
     await mongoose.connect(MONGO_URI, {
       dbName: 'job-board',
     });
